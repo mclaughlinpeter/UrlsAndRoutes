347e0614f6dc6d6acd53306f2cfea937af681b2c
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-12-03 09:18:43 -0500

    Add initial CustomerController.cs class to Controllers folder

---
+++ UrlsAndRoutes/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UrlsAndRoutes.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public ViewResult Index() => View("Result", new Result
+        {
+            Controller = nameof(CustomerController),
+            Action = nameof(Index)
+        });
+
+        public ViewResult List() => View("Result", new Result
+        {
+            Controller = nameof(CustomerController),
+            Action = nameof(List)
+        });
+    }
+}
