32985ee83e17d3d7a9f2ea3fbf7bf1e9a58ce338
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-12-03 09:20:42 -0500

    Add AdminController.cs to Controllers folder

---
+++ UrlsAndRoutes/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UrlsAndRoutes.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ViewResult Index() => View("Result", new Result
+        {
+            Controller = nameof(AdminController),
+            Action = nameof(Index)
+        });
+    }
+}
