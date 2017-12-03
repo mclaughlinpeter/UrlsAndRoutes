d2f8c1109df0415e8dbc8f79474567f277f40710
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-12-03 09:15:20 -0500

    Add Controllers folder and add initial HomeController.cs controller with Index() action method

---
+++ UrlsAndRoutes/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UrlsAndRoutes.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() => View("Result", new Result
+        {
+            Controller = nameof(HomeController),
+            Action = nameof(Index)
+        });
+    }
+}
