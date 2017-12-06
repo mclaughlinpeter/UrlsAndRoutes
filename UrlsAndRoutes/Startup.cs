9713b873530877f6027f3347676ecb9a1680a47c
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-12-05 19:19:40 -0500

    Added alias from Shop controller (non-existant) to Home controller

--- UrlsAndRoutes/Startup.cs
+++ UrlsAndRoutes/Startup.cs
@@ -25,6 +25,10 @@ namespace UrlsAndRoutes
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "ShopSchema",
+                    template: "Shop/{action}",
+                    defaults: new { controller = "Home" });
                 routes.MapRoute("", "X{controller}/{action}");
                 routes.MapRoute(
                     name: "default", 
