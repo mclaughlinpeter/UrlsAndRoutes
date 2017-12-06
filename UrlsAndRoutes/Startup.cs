8afd769b8dca4499c3ada94d5a9dd49c3378f1d4
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-12-05 19:40:00 -0500

    Add custom URL segment variable 'id' and CustomVariable controller in HomeController.cs class

--- UrlsAndRoutes/Startup.cs
+++ UrlsAndRoutes/Startup.cs
@@ -26,20 +26,8 @@ namespace UrlsAndRoutes
             app.UseStaticFiles();
             app.UseMvc(routes => {
                 routes.MapRoute(
-                    name: "ShopSchema2",
-                    template: "Shop/OldAction",
-                    defaults: new { controller = "Home", action = "Index" });
-                routes.MapRoute(
-                    name: "ShopSchema",
-                    template: "Shop/{action}",
-                    defaults: new { controller = "Home" });
-                routes.MapRoute("", "X{controller}/{action}");
-                routes.MapRoute(
-                    name: "default", 
-                    template: "{controller=Home}/{action=Index}");
-                routes.MapRoute(
-                    name: "",
-                    template: "Public/{controller=Home}/{action=Index}");
+                    name: "MyRoute",
+                    template: "{controller=Home}/{action=Index}/{id=DefaultId}");
             });
         }
     }
