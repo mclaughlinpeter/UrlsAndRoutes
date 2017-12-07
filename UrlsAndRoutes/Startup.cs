a50476e4884c81fdc0c30f02a922ff2996caa3b6
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-12-07 17:10:10 -0500

    Changed WeekDayConstraint route to inline in Startup.cs class

--- UrlsAndRoutes/Startup.cs
+++ UrlsAndRoutes/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using UrlsAndRoutes.Infrastructure;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Routing.Constraints;
 
 namespace UrlsAndRoutes
 {
@@ -16,6 +18,8 @@ namespace UrlsAndRoutes
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<RouteOptions>(options =>
+                options.ConstraintMap.Add("weekday", typeof(WeekDayConstraint)));
             services.AddMvc();
         }
 
@@ -28,9 +32,7 @@ namespace UrlsAndRoutes
             app.UseMvc(routes => {
                 routes.MapRoute(
                     name: "MyRoute",
-                    template: "{controller}/{action}/{id?}",
-                    defaults: new { controller = "Home", action = "Index" },
-                    constraints: new { id = new WeekDayConstraint() });
+                    template: "{controller=Home}/{action=Index}/{id:weekday?}");
             });
         }
     }
