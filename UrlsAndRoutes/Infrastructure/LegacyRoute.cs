cda9553120f8f9e67c47dbefbc1a2049facb48cf
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-12-11 16:59:23 -0500

    Add LegacyRoute.cs class in Infrastructure folder

---
+++ UrlsAndRoutes/Infrastructure/LegacyRoute.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System.Text;
+
+namespace UrlsAndRoutes.Infrastructure
+{
+    public class LegacyRoute : IRouter
+    {
+        private string[] urls;
+
+        public LegacyRoute(params string[] targetUrls)
+        {
+            this.urls = targetUrls;
+        }
+
+        public VirtualPathData GetVirtualPath(VirtualPathContext context)
+        {
+            return null;
+        }
+
+        public Task RouteAsync(RouteContext context)
+        {
+            string requestedUrl = context.HttpContext.Request.Path.Value.TrimEnd('/');
+
+            if (urls.Contains(requestedUrl, StringComparer.OrdinalIgnoreCase))
+            {
+                context.Handler = async ctx =>
+                {
+                    HttpResponse response = ctx.Response;
+                    byte[] bytes = Encoding.ASCII.GetBytes($"URL: {requestedUrl}");
+                    await response.Body.WriteAsync(bytes, 0, bytes.Length);
+                };
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
