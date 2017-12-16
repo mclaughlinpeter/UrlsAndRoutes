2d05f1f9ad8087672d8bb214bf671d34db947a0f
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-12-16 12:30:18 -0500

    Add area route to the Startup class

--- UrlsAndRoutes/Startup.cs
+++ UrlsAndRoutes/Startup.cs
@@ -34,17 +34,19 @@ namespace UrlsAndRoutes
             app.UseStaticFiles();
             app.UseMvc(routes =>
             {
-                //routes.MapRoute(
-                //    name: "NewRoute",
-                //    template: "App/Do{action}",
-                //    defaults: new { controller = "Home" });
+                routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}");
+
                 routes.Routes.Add(new LegacyRoute(
                     app.ApplicationServices,
                     "/articles/Windows_3.1_Overview.html",
                     "/old/.NET_1.0_Class_Library"));
+
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
                 routes.MapRoute(
                     name: "out",
                     template: "outbound/{controller=Home}/{action=Index}");
